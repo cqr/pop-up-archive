c0d0038bdc3d77358a4a2d3689e279e19d29e9eb
chrisrhoden <carhoden@gmail.com>
2013-01-17 16:24:47 -0500

    complete setup of login with prx

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,28 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
+
+  def self.find_for_prx_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => "#{auth.provider}::#{auth.uid.to_s}").first
+    unless user
+      user = User.create(
+        name: "#{auth.info.first_name} #{auth.info.last_name}",
+        provider: auth.provider,
+        uid: "#{auth.provider}::#{auth.uid.to_s}",
+        email: auth.info.email,
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.prx_data"] && session["devise.prx_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.name  = "#{data["first_name"]} #{data["last_name"]}"
+      end
+    end
+  end
 end
