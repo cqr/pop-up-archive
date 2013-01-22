d50b40c7657085e15fac46a67c99762e31bab190
chrisrhoden <carhoden@gmail.com>
2013-01-22 11:40:25 -0500

    load the env file in ruby as part of boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,20 @@
 require 'rubygems'
 
+# Set up the environment
+ENV['ENV_OVERRIDE_FILE'] ||= File.expand_path('../env_vars', __FILE__)
+
+if File.exists?(ENV['ENV_OVERRIDE_FILE'])
+  File.open(ENV['ENV_OVERRIDE_FILE']) do |file|
+    while line = file.gets
+      line.chomp!
+      var, val = line.split('=', 2)
+      ENV[var] = val
+    end
+  end
+end
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
