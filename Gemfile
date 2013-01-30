737b3f3597c6bdbc5379dddcc8098fd7ca7eb8c4
chrisrhoden <carhoden@gmail.com>
2013-01-30 10:43:38 -0500

    update carrierwave to work on heroku

--- Gemfile
+++ Gemfile
@@ -74,3 +74,4 @@ gem 'geocoder'
 gem 'will_paginate'
 
 gem 'carrierwave'
+gem 'fog'
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
+  config.fog_credentials = {
+        :provider               => 'AWS',       
+        :aws_access_key_id      => ENV['UPLOAD_S3_ACCESS_KEY_ID'],       
+        :aws_secret_access_key  => ENV['UPLOAD_S3_ACCESS_KEY']
+      }
+
+  config.fog_directory  = ENV['UPLOAD_S3_ACCESS_KEY']
+  config.fog_public     = false
+end
