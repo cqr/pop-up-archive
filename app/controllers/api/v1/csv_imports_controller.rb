5dd00e156aaf982aab6a026ffff902c7e0f44bb5
chrisrhoden <carhoden@gmail.com>
2013-02-01 10:25:01 -0500

    clean up the csv_imports controller and bring us back to spec

--- app/controllers/api/v1/csv_imports_controller.rb
+++ app/controllers/api/v1/csv_imports_controller.rb
@@ -2,12 +2,7 @@ class Api::V1::CsvImportsController < Api::V1::BaseController
   expose(:csv_import)
 
   def create
-    if csv_import.save
-      # redirect_to(api_csv_import_path(csv_import))
-      respond_with(:api, csv_import)
-    else
-      respond_with(:api, csv_import)
-    end
-    # respond_with(:api, csv_import, :location => api_csv_import_path(csv_import))
+    csv_import.save
+    respond_with(:api, csv_import)
   end
 end
