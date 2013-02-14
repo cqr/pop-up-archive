f8f1b01a2a210a4c5ab17b538bf207b7809287af
chrisrhoden <carhoden@gmail.com>
2013-02-13 19:43:26 -0500

    hopefully solve the problem where the logout is happening on a different window

--- app/assets/javascripts/angular/modules/rails-ujs.js
+++ app/assets/javascripts/angular/modules/rails-ujs.js
@@ -5,10 +5,11 @@ angular.module('RailsUjs', [])
       el.bind('click', function (e) {
         e.stopPropagation();
         e.preventDefault();
-
-        angular.element("<form target='" + attrs.href +
+        var form = angular.element("<div style='display:none'><form target='" + attrs.href +
           "' method='POST'><input type='hidden' name='_method' value='" + attrs.method +
-          "' ></form>")[0].submit();
+          "' ></form></div>")
+        el.parent().append(form);
+        form.children()[0].submit();
       });
     }
   }
