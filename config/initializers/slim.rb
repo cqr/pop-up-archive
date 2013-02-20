2edde6d875f19326f18a0f7b77420ca9969d7e56
chrisrhoden <carhoden@gmail.com>
2013-02-20 10:21:22 -0500

    remove these helpers

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -2,8 +2,3 @@ Slim::Engine.set_default_options :pretty => true unless Rails.env.production?
 Rails.application.assets.register_mime_type 'text/html', '.html'
 Rails.application.assets.register_engine '.slim', Slim::Template
 Sprockets.register_engine '.slim', Slim::Template
-module Sprockets::Helpers::RailsHelper
-  def render_partial(*args)
-    "Gooooood."
-  end
-end
