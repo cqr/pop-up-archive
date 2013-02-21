019f090fd1d2d519484a26a6d7ce5db2ddb7dcc3
chrisrhoden <carhoden@gmail.com>
2013-02-21 17:02:38 -0500

    show total number of hits

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::SearchesController < Api::V1::BaseController
   def show
     query_builder = QueryBuilder.new(params)
+    page = params[:page].to_i
 
     @search = Item.search do
+
+
+      if page.present? && page > 1
+        from (page - 1) * 25
+      end
+      size 25
+
       query_builder.query do |q|
         query &q
       end
@@ -16,6 +24,8 @@ class Api::V1::SearchesController < Api::V1::BaseController
       end
 
     end
+
+    Rails.logger.debug(@search.inspect)
 
     respond_with @search
   end
--- app/views/api/v1/searches/show.json.rabl
+++ app/views/api/v1/searches/show.json.rabl
@@ -1,12 +1,13 @@
 object false
 
 node(:facets, if: @search.facets.present? )  { @search.facets }
-
+node(:total_hits) { @search.total }
+node(:page) { ((@search.options[:from] || 0) / 25) + 1 }
 node(:max_score) { @search.max_score }
 node(:query) { params[:query] }
 
 node 'results' do
-  @search.results.map do |result|
+  @search.results.to_a.map do |result|
     partial('api/v1/items/item', object: result)
   end
 end
