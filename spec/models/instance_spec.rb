55f0725bc724c7bf6b49eda70fde3f98d81ed06d
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-02-26 11:55:48 -0500

    fix instance spec

---
+++ spec/models/instance_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Instance do
+
+  it "should allow create with valid attributes" do
+    instance = Instance.new
+    instance.digital = true
+    instance.format = 'audio/wav'
+	instance.location = 'http://this.is.an.example.com'
+    instance.identifier = 'http://this.is.an.example.com'
+    instance.save.should be_true
+  end
+
+end
