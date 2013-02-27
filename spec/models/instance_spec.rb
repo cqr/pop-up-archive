51ed2cc255234a553706cc332b84d3c928a9db47
Chris Kalafarski <chris@farski.com>
2013-02-27 14:59:10 -0500

    Whitespace

--- spec/models/instance_spec.rb
+++ spec/models/instance_spec.rb
@@ -6,7 +6,7 @@ describe Instance do
     instance = Instance.new
     instance.digital = true
     instance.format = 'audio/wav'
-	instance.location = 'http://this.is.an.example.com'
+    instance.location = 'http://this.is.an.example.com'
     instance.identifier = 'http://this.is.an.example.com'
     instance.save.should be_true
   end
