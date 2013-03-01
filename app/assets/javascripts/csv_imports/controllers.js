9cc36d5a9c1fe0f4c7b4b65653bc586c93f4aeda
Chris Kalafarski <chris@farski.com>
2013-03-01 16:20:01 -0500

    Manual file select import

--- app/assets/javascripts/csv_imports/controllers.js
+++ app/assets/javascripts/csv_imports/controllers.js
@@ -85,3 +85,38 @@ angular.module('Directory.csvImport.controllers', ['Directory.alerts', 'Director
 .controller('ImportsCtrl', ['$scope', 'CsvImport', 'Loader', function ($scope, CsvImport, Loader) {
   Loader.page(CsvImport.query(), 'Imports', $scope);
 }])
+.controller('FSImportCtrl', ['$scope', '$http', 'CsvImport', '$timeout', 'Alert', function ($scope, $http, CsvImport, $timeout, Alert) {
+  $scope.setFile = function(element) {
+    $scope.$apply(function($scope) {
+      $scope.files = element.files;
+    });
+  };
+
+  $scope.submit = function () {
+    angular.forEach($scope.files, function(file) {
+      var alert = new Alert();
+
+      alert.status = "Uploading";
+      alert.progress = 1;
+      alert.message = file.name;
+      alert.add();
+
+      var fData = new FormData();
+      fData.append('csv_import[file]', file);
+
+      $http({
+        method: 'POST',
+        url: '/api/csv_imports',
+        data: fData,
+        headers: { "Content-Type": undefined },
+        transformRequest: angular.identity
+      }).success(function(data, status, headers, config) {
+        var csvImport = new CsvImport({id:data.id});
+        alert.progress = 25;
+        alert.status = "Waiting";
+        alert.startSync(csvImport.alertSync());
+      });
+    });
+  };
+}])
+
