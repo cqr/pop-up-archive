b0cb82fb94791b4933d5fc70d883e50a072fd815
Chris Kalafarski <chris@farski.com>
2013-03-05 15:45:57 -0500

    Authenticate items controller

--- app/assets/javascripts/items/controllers.js
+++ app/assets/javascripts/items/controllers.js
@@ -1,8 +1,11 @@
-angular.module('Directory.items.controllers', ['Directory.loader', 'Directory.items.models'])
-.controller('ItemsCtrl', [ '$scope', 'Item', 'Loader', function ItemsCtrl($scope, Item, Loader) {
-  $scope.items = Loader.page(Item.query(), 'Items');
+angular.module('Directory.items.controllers', ['Directory.loader', 'Directory.user', 'Directory.items.models'])
+.controller('ItemsCtrl', [ '$scope', 'Item', 'Loader', 'Me', function ItemsCtrl($scope, Item, Loader, Me) {
+  Me.authenticated(function (data) {
+    $scope.items = Loader.page(Item.query(), 'Items');
+  });
 }])
 .controller('ItemCtrl', ['$scope', 'Item', 'Loader', '$routeParams', function ItemCtrl($scope, Item, Loader, $routeParams) {
+
   if ($routeParams.id) {
     Loader.page(Item.get($routeParams.id), 'Item/'+$routeParams.id, $scope);
   }
