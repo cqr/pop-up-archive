06df5e98c84fd070939fa233a11075a40564737c
chrisrhoden <carhoden@gmail.com>
2013-03-10 21:03:59 -0400

    logging

--- app/workers/csv_import_worker.rb
+++ app/workers/csv_import_worker.rb
@@ -2,8 +2,11 @@ class CsvImportWorker
   include Sidekiq::Worker
 
   def perform(import_id)
+    logger.debug("starting import #{import_id}")
     import = CsvImport.find(import_id)
+    logger.debug("Got that import")
     import.process!
+    logger.debug("Process!")
   rescue Exception => e
     import.error!(e)
   end
