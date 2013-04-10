5f9b058b6d4712a5f68e65c13bb5acbca6c295c9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-04-10 01:41:09 -0400

    get rid of error requesting items for no collection

--- app/assets/javascripts/items/controllers.js
+++ app/assets/javascripts/items/controllers.js
@@ -1,7 +1,9 @@
 angular.module('Directory.items.controllers', ['Directory.loader', 'Directory.user', 'Directory.items.models'])
 .controller('ItemsCtrl', [ '$scope', 'Item', 'Loader', 'Me', function ItemsCtrl($scope, Item, Loader, Me) {
   Me.authenticated(function (data) {
-    $scope.items = Loader.page(Item.query(), 'Items');
+    if ($scope.collectionId) {
+      $scope.items = Loader.page(Item.query(), 'Items');
+    }
   });
 }])
 .controller('ItemCtrl', ['$scope', 'Item', 'Loader', 'Me', '$routeParams', function ItemCtrl($scope, Item, Loader, Me, $routeParams) {
