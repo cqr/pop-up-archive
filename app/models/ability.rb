34d0500275ba0e0bcae3be62ec39fee0152d29ce
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-04-11 16:25:00 -0400

    fix when there is no user specified

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, Item, collection: { id: user.collection_ids }
+    can :manage, Item, collection: { id: (user ? user.collection_ids : []) }
     can :read, Item
   end
 end
