a2c652471ecc985748d450b106a8852ddcbbfd66
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-04-25 16:32:01 -0400

    fix logo tagline, fix analysis processing

--- app/workers/download_analysis_worker.rb
+++ app/workers/download_analysis_worker.rb
@@ -6,12 +6,12 @@ class DownloadAnalysisWorker
   include Sidekiq::Worker
 
   def perform(audio_file_id, analysis_url)
-    connection = Fog::Storage.new(audio_file.file.fog_credentials)
+    audio_file = AudioFile.find(audio_file_id)
+    file = audio_file.file
+    connection = Fog::Storage.new(file.fog_credentials)
     uri = URI.parse(transcript_url)
     analysis = Utils.download_private_file(connection, uri)
-
-    audio_file = AudioFile.find(audio_file_id)
-    audio_file.item.process_analysis(analysis)    
+    item.process_analysis(analysis)
   end
 
 end
