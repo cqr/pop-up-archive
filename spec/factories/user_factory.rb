4ec820aa1b96856ae335ce9ea9e6847fa6b0f668
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-05-06 19:09:09 -0400

    fix tests, fix layout of login pages

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "email#{n}@example.com" }
     password "foo123"
+    sequence(:invitation_token) {|n| "invitation_token_#{n}" }
   end
 end
