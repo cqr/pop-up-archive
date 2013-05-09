6161df1d1fa79b00e8de4dcaa4066b86237fe5ed
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-05-08 21:24:32 -0400

    fix tests, user must have a name now

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    name "test"
     sequence(:email) {|n| "email#{n}@example.com" }
     password "foo123"
     sequence(:invitation_token) {|n| "invitation_token_#{n}" }
