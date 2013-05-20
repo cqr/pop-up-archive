395dfcf0df1bbcff1d779358c64b78ba14337f71
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-05-20 16:59:21 -0400

    change dashboard/hompage

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Item
+    can :read,   Collection
+    can :create, Collection
+    can :manage, Collection, id: (user ? user.collection_ids : [])
+
+    can :read,   Item
     can :manage, Item, collection: { id: (user ? user.collection_ids : []) }
 
-    can :read, Entity
-    # can :manage, [Entity] {|entity| user.collection_ids.include?(entity.item.collection_id)}
+    can :read,   Entity
     can :manage, Entity, item: { collection: { id: (user ? user.collection_ids : []) }}
   end
 end
