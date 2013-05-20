c7e080035b9a9006150ee369d78e2cb943311de4
chrisrhoden <carhoden@gmail.com>
2013-05-20 16:54:25 -0400

    starting mockups

--- app/assets/javascripts/collections/models.js
+++ app/assets/javascripts/collections/models.js
@@ -1,3 +1,4 @@
+;(function(){
 angular.module('Directory.collections.models', ['RailsModel'])
 .factory('Collection', ['Model', function (Model) {
   var Collection = Model({url:'/api/collections/{{id}}', name: 'collection'});
@@ -9,4 +10,27 @@ angular.module('Directory.collections.models', ['RailsModel'])
   Collection.attrAccessible = ['title', 'description', 'itemsVisibleByDefault'];
 
   return Collection;
-}]);
+}])
+.filter('publicCollections', function () {
+  return buildCollectionFilter(true);
+})
+.filter('privateCollections', function() {
+  return buildCollectionFilter(false);
+});
+
+function buildCollectionFilter(visible) {
+  return function(collections) {
+    if (angular.isArray(collections)) {
+      var publicCollections = [];
+      angular.forEach(collections, function(collection) {
+        if (collection.itemsVisibleByDefault == visible) {
+          publicCollections.push(collection);
+        }
+      });
+      return publicCollections
+    } else {
+      return collections;
+    }
+  };
+}
+})();
