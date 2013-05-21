02e21d3f1eaf5a23351e33692dd8e7f175b97241
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-05-21 14:04:34 -0400

    add contribution to ability

---
+++ app/controllers/api/v1/contributions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ContributionsController < Api::V1::BaseController
+  expose :item
+  expose :contributions, ancestor: :item
+  expose :contribution
+
+  authorize_resource decent_exposure: true
+
+  def update
+    contribution.save
+    respond_with :api, contribution
+  end
+
+  def destroy
+    contribution.destroy
+    respond_with :api, contribution
+  end
+
+end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,5 +11,8 @@ class Ability
 
     can :read,   Entity
     can :manage, Entity, item: { collection: { id: (user ? user.collection_ids : []) }}
+    
+    can :read,   Contribution
+    can :manage, Contribution, item: { collection: { id: (user ? user.collection_ids : []) }}
   end
 end
