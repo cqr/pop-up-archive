8905aa2d0400ae65549bb6f399bdf9f0864264af
Ignacio Perez <iaperez@gmail.com>
2013-06-25 18:29:32 -0700

    xml importer for non pbcore xml files (BBG,Will-Illinois,OpenVault)

--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -15,4 +15,23 @@ namespace :import do
     importer.import_omeka_collection
   end
 
+  desc "Import Open Vault xml files from a directory"
+  task :import_xml_openvault_dir, [:collection_id, :dir] => [:environment] do |t, args|
+    importer = XMLMediaImporter.new(collection_id: args.collection_id, dir: args.dir)
+    importer.import_openvault_directory
+  end
+
+  desc "Import Illinois Collection XML file "
+  task :import_xml_illinois_collection, [:collection_id, :file] => [:environment] do |t, args|
+    importer = XMLMediaImporter.new(collection_id: args.collection_id, file: args.file)
+    importer.import_xml_illinois_collection
+  end
+
+  desc "Import BBG Collection XML file "
+  task :import_xml_bbg_feed, [:collection_id, :file] => [:environment] do |t, args|
+    importer = XMLMediaImporter.new(collection_id: args.collection_id, file: args.file)
+    importer.import_xml_bbg_feed
+  end
+
+
 end
---
+++ lib/xml_media_importer.rb
@@ -0,0 +1,137 @@
+require 'nokogiri'
+
+class XMLMediaImporter
+
+  attr_accessor :file, :collection, :dir
+
+  def initialize(options={})
+    #PBCore.config[:date_formats] = ['%m/%d/%Y', '%Y-%m-%d']
+
+    self.collection = Collection.find(options[:collection_id])
+    directory = options[:dir] ||= nil
+
+    if directory == nil
+      raise "File missing or 0 length: #{options[:file]}" unless (File.size?(options[:file]).to_i > 0)
+      self.file = File.open(options[:file])
+      #self.dir = Dir[options[:file]+"*.xml"]
+    else
+      self.dir = Dir[directory+"/*.xml"]
+    end
+
+  end
+
+  def is_audio_file?(url)
+    puts "is_audio_file? url:#{url}"
+    uri = URI.parse(url)
+    ext = (File.extname(uri.path)[1..-1] || "").downcase
+    ['mp3', 'wav', 'mp2', 'aac'].include?(ext)
+  end
+
+  def import_xml_illinois_collection
+   xmlFeed=Nokogiri::XML(file)
+   xmlFeed.remove_namespaces!
+   descriptions = xmlFeed.xpath("//PBCoreCollection/PBCoreDescriptionDocument")
+   descriptions.each do |pbcoreDescription|
+     item_for_illinois_doc(pbcoreDescription).save!
+   end
+  end
+
+  def import_xml_bbg_feed
+    # pbc_collection = PBCore::V2::Collection.parse(file)
+    xmlFeed=Nokogiri::XML(file)
+    xmlFeed.remove_namespaces!
+    items = xmlFeed.xpath("//item")
+    items.each do |item|
+      item_for_bbg(item).save!
+    end
+  end
+
+
+  def import_openvault_directory
+    # pbc_collection = PBCore::V2::Collection.parse(file)
+    dir.each do |xmlfile|
+      file =  File.open(xmlfile.to_s)
+      xmlFeed=Nokogiri::XML(file)
+      #to simplify xpath queries, try to remove namespaces
+      #it might not work for files with multiples namespaces
+      xmlFeed.remove_namespaces!
+      item_for_openvault(xmlFeed).save!
+    end
+  end
+
+
+  def item_for_illinois_doc(doc)
+    item                   = Item.new
+    item.collection        = collection
+    item.title             = doc.search('title')[0].text
+    item.tags              = doc.search('subject').collect{|s| s.text}.compact
+    item.description       = doc.xpath("pbcoreDescription[descriptionType='Abstract']/description").text
+    item.physical_location = doc.xpath("pbcoreCoverage[coverageType='Spatial']/coverage").text
+    item.creators          = doc.xpath("pbcoreCreator/creator").collect{|s| Person.for_name(s.text)}
+    item.contributions     = doc.xpath("pbcoreContributor").collect{|s| Contribution.new(person:Person.for_name(s.xpath("contributor").text),role:s.xpath("contributorRole").text)}
+    # files are not working on illions feed.
+    item
+  end
+
+  def item_for_openvault(doc)
+    item                   = Item.new
+    item.collection        = collection
+    #Alternative xpath, only works for some files doc.xpath("//pbcoreTitle[@titleType = 'Series']").text
+    item.title             =  doc.xpath("//pbcoreTitle[not(@titleType='Episode')]").collect{|s| s.text}.join(" ")[0,255]
+    item.tags              = doc.xpath("//pbcoreSubject").collect{|s| s.text[0,255]}.compact || []
+    item.physical_location = doc.xpath("pbcoreCoverage[coverageType='Spatial']/coverage").text
+    item.description       = doc.xpath("//pbcoreDescription[@descriptionType='Description']").text
+    #item.creators         = doc.xpath("credit").collect{|s| Person.for_name(s.text)}
+    item.contributions     = doc.xpath("pbcoreContributor").collect{|s| Contribution.new(person:Person.for_name(s.xpath("contributor").text),role:s.xpath("contributorRole").text)}
+    mediaContents          =   doc.xpath("//pbcoreInstantiation[not(instantiationPhysical)]")
+    mediaContents.each do |mediaContent|
+      url                  = mediaContent.xpath("instantiationLocation").text
+      next unless is_audio_file?(url)
+      audio                = AudioFile.new
+      instance             = item.instances.build
+      instance.digital     = true
+      #instance.format     = pbcInstance.try(:digital).try(:value)
+      #instance.identifier = pbcInstance.detect_element(:identifiers)
+      #instance.location   = pbcInstance.location
+      audio = AudioFile.new
+      instance.audio_files << audio
+      item.audio_files << audio
+      audio.identifier     = url
+      audio.remote_file_url= url
+      #audio.format        = pbcPart.try(:digital).try(:value) || instance.format
+      #audio.size          = mediaContent.attribute('fileSize').value
+    end
+    item
+  end
+
+  def item_for_bbg(doc)
+    item                   = Item.new
+    item.collection        = collection
+    item.title             = doc.xpath("title").text
+    item.tags              = doc.xpath("keywords").collect{|s| s.text.split(/,|;/)}.flatten.compact.delete_if(&:empty?)
+    item.tags.concat(doc.xpath("category").collect{|s| s.text}.compact.delete_if(&:empty?))
+    item.description       = doc.xpath("description").text
+    # VOA is not a person, but it is the closest thing to a creator on the BBG file
+    item.creators          = doc.xpath("credit").collect{|s| Person.for_name(s.text)}
+    #item.contributions     = doc.xpath("pbcoreContributor").collect{|s| Contribution.new(person:Person.for_name(s.xpath("contributor").text),role:s.xpath("contributorRole").text)}
+    mediaContents =   doc.xpath("group/content")
+    mediaContents.each do |mediaContent|
+        url = mediaContent.attribute('url').value
+        next unless is_audio_file?(url)
+        audio = AudioFile.new
+        instance = item.instances.build
+        instance.digital    = true
+        #instance.format     = pbcInstance.try(:digital).try(:value)
+        #instance.identifier = pbcInstance.detect_element(:identifiers)
+        #instance.location   = pbcInstance.location
+        audio = AudioFile.new
+        instance.audio_files << audio
+        item.audio_files << audio
+        audio.identifier        = url
+        audio.remote_file_url   = url
+        #audio.format            = pbcPart.try(:digital).try(:value) || instance.format
+        audio.size              = mediaContent.attribute('fileSize').value
+    end
+    item
+  end
+end
