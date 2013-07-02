f8fcee7a171d67f73f209dbf20e7cefe463bae23
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-07-02 15:23:56 -0400

    fix AR use in migration to account for paranoid records, make travis happier

--- db/migrate/20130619154837_fix_visibility_and_storage_configuration.rb
+++ db/migrate/20130619154837_fix_visibility_and_storage_configuration.rb
@@ -5,10 +5,10 @@ class FixVisibilityAndStorageConfiguration < ActiveRecord::Migration
     execute("UPDATE storage_configurations SET is_public = 'f' where is_public is null AND provider = 'AWS'")
 
     # set all the collections to have default and upload storage
-    Collection.find_each{|c| c.set_storage; c.save!}
+    Collection.with_deleted.find_each{|c| c.set_storage; c.save!}
 
     # update the items to get rid of dupe storage config
-    Item.find_each{|i|
+    Item.with_deleted.find_each{|i|
       if i.storage_configuration && i.collection && (i.storage_configuration.key == i.collection.default_storage.key)
         i.storage_configuration.destroy
       end
