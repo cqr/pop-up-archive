1fb2d1f14502ae80460c4bb0dfc5ca734716a8eb
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-07-16 12:39:34 -0400

    fix upload button to properly size and reposition

--- app/assets/javascripts/angular/modules/upload-button.js
+++ app/assets/javascripts/angular/modules/upload-button.js
@@ -1,37 +1,44 @@
 angular.module('fileButton', [])
-.directive('fileButton', [function () {
+.directive('fileButton', ['$timeout', function ($timeout) {
   return {
     link: function(scope, element, attributes) {
 
-      var el = angular.element(element)
-      var button = el.children()[0]
+      var el = angular.element(element);
+      var button = angular.element(el.children()[0]);
 
       el.css({
         position: 'relative',
-        overflow: 'hidden',
-        width: button.offsetWidth,
-        height: button.offsetHeight
-      })
+        overflow: 'hidden'
+      });
 
-      var fileInput = angular.element('<input type="file" multiple />')
+      button.bind('mouseover', function (event) {                
+        el.css({
+          display: 'inline-block',
+          width: button[0].offsetWidth,
+          height: button[0].offsetHeight
+        });
+      });
+
+      var fileInput = angular.element('<input type="file" multiple />');
       fileInput.css({
         position: 'absolute',
         top: 0,
         left: 0,
         'z-index': '2',
-        marginTop: '2px',
+        marginTop: '0px',
         padding: '0',
         width: '100%',
         height: '100%',
         opacity: '0',
         cursor: 'pointer'
-      })
+      });
 
       fileInput.bind('change', function() {
         scope.setFiles(fileInput);
+        fileInput[0].value = "";
       });
 
-      el.append(fileInput)
+      el.append(fileInput);
     }
   }
 }]);
