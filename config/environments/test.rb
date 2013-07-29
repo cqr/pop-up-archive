fc512571f1d954bc29bc00017ae21009b2c93ecf
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-07-29 16:08:13 -0400

    fix test env to have default urls so tests work

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ PopUpArchive::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'test.popuparchive.org' }
+  Rails.application.routes.default_url_options = { host: 'test.popuparchive.org' } 
 end
