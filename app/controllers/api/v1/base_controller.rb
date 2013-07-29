32d2aa003eab0d3b9d997975f977cec447eb2f8a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-07-29 16:04:02 -0400

    commented out, but this is how to add oauth protection to api calls

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::BaseController < Api::BaseController
   respond_to :json
+
+  # this should protect the resources using oauth when user is not logged in (i.e. API requests)
+  # doorkeeper_for :create, :update, :destroy, if: lambda{|c| !current_user }
+
 end
