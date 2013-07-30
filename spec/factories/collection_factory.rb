e51ef48914ad271a4e66aecf39c3b32bf24c45ea
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-07-30 14:46:34 -0400

    fix collection change bug on changingn is_public

--- spec/factories/collection_factory.rb
+++ spec/factories/collection_factory.rb
@@ -2,8 +2,9 @@ FactoryGirl.define do
   factory :collection do
 
     title "test collection"
-    association :default_storage, factory: :storage_configuration_private
     items_visible_by_default true
+    association :default_storage, factory: :storage_configuration_public
+    association :upload_storage, factory: :storage_configuration_private
   
     factory :collection_public do
       items_visible_by_default true
