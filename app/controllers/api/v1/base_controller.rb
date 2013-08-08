f362b2e9d64185e0f8e4db12fade8d46450ee824
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-08-08 16:19:39 -0400

    remove cancan check as it is blocking fixer updates

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::BaseController < Api::BaseController
   # this should protect the resources using oauth when user is not logged in (i.e. API requests)
   # doorkeeper_for :create, :update, :destroy, if: lambda{|c| !current_user }
 
-  expose(:items) { Item.visible_to_user current_user }
-  expose(:collections) { Collection.visible_to_user current_user }
+  # expose(:items) { Item.visible_to_user current_user }
+  # expose(:collections) { Collection.visible_to_user current_user }
 end
