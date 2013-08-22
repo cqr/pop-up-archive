6158c7bd31317bcac209bf5f6354ba6e00f81469
Ignacio Perez <iaperez@gmail.com>
2013-08-22 15:38:38 -0700

    soundcloud public importer (url testing)

---
+++ lib/soundcloud_backend.rb
@@ -0,0 +1,43 @@
+class SoundcloudBackend
+  def self.get_url
+    client = Soundcloud.new({
+                                :client_id      => ENV['SOUNDCLOUD_SERVER_APP_ID'] ,
+                                :client_secret  =>  ENV['SOUNDCLOUD_SERVER_APP_SECRET'],
+                                :redirect_uri =>  ENV['SOUNDCLOUD_POP_REDIRECT_URL']
+                            })
+    puts client.authorize_url()
+  end
+
+  def self.import_url(soundcloud_url, collection_id)
+    collection = Collection.find(collection_id)
+    client = Soundcloud.new(:client_id =>  ENV['SOUNDCLOUD_SERVER_APP_ID'] ,:access_token => ENV['SOUNDCLOUD_POPUPARCHIVE_USER_ACCESS_TOKEN'])
+    station = client.get('/resolve', :url => soundcloud_url, client_id: ENV['SOUNDCLOUD_SERVER_APP_ID'])
+    tracks = client.get("/users/#{station.id}/tracks")
+
+    tracks.each do |track|
+      if track.downloadable
+        item = Item.new
+        item.collection = collection
+        item.date_created = track.created_at
+        item.title = track.title
+        item.description = track.description
+
+        instance = item.instances.build
+        instance.digital    = true
+        instance.format     = track.original_formar
+        instance.identifier = track.id
+        instance.location   = track.permalink_url
+
+        audio = AudioFile.new
+        instance.audio_files << audio
+        item.audio_files << audio
+        audio.identifier        =  track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
+        audio.remote_file_url   =  track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
+        item.save!
+      end
+    end
+
+
+  end
+
+end
---
+++ lib/tasks/soundcloud.rake
@@ -0,0 +1,16 @@
+require 'pb_core'
+
+namespace :soundcloud do
+
+  desc "Generate link authorization url"
+  task :generate_link => :environment do
+    SoundcloudBackend.get_url
+  end
+
+  desc "Import PBCore 2.0 pbcoreDescriptionDocument XML file from Omeka"
+  task :import_url, [:collection_id, :url] => [:environment] do |t, args|
+    SoundcloudBackend.import_url(args.url, args.collection_id)
+  end
+
+
+end
