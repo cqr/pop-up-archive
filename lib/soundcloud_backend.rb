63036950111f45e4d949f5b608a3cb2d13cf26c7
Ignacio Perez <iaperez@gmail.com>
2013-08-23 00:29:58 -0700

    test for soundcloud importer

--- lib/soundcloud_backend.rb
+++ lib/soundcloud_backend.rb
@@ -24,7 +24,7 @@ class SoundcloudBackend
 
         instance = item.instances.build
         instance.digital    = true
-        instance.format     = track.original_formar
+        instance.format     = track.original_format
         instance.identifier = track.id
 
         audio = AudioFile.new
