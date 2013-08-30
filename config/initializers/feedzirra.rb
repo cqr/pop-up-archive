789abf18ce8ec4de4ed76876401e14a5beacc036
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-08-30 11:19:25 -0400

    Revert "Writing a new feed class for iTunes RSS"
    This reverts commit fa5cd0de53828f62ce94d82f888d11d75e1c027a.

--- config/initializers/feedzirra.rb
+++ config/initializers/feedzirra.rb
@@ -2,4 +2,3 @@ require 'feedzirra'
 
 Feedzirra::Feed.add_feed_class(Feedzirra::Parser::MediaRSS)
 Feedzirra::Feed.add_feed_class(Feedzirra::Parser::MediaRSSFeedBurner)
-Feedzirra::Feed.add_feed_class(Feedzirra::Parser::ItunesRSS)
