4562c54354ef826bcd6707001e3bfdfb89f65059
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-09-05 18:14:30 -0400

    cleanup sc backend, prevent dupes

--- lib/soundcloud_backend.rb
+++ lib/soundcloud_backend.rb
@@ -1,40 +1,45 @@
 class SoundcloudBackend
+
   def self.get_url
-    client = Soundcloud.new({
-                                :client_id      => ENV['SOUNDCLOUD_SERVER_APP_ID'] ,
-                                :client_secret  =>  ENV['SOUNDCLOUD_SERVER_APP_SECRET'],
-                                :redirect_uri =>  ENV['SOUNDCLOUD_POP_REDIRECT_URL']
-                            })
+    client = soundcloud_client
     puts client.authorize_url()
   end
 
   def self.import_url(soundcloud_url, collection_id)
     collection = Collection.find(collection_id)
-    client = Soundcloud.new(:client_id =>  ENV['SOUNDCLOUD_SERVER_APP_ID'] ,:access_token => ENV['SOUNDCLOUD_POPUPARCHIVE_USER_ACCESS_TOKEN'])
-    station = client.get('/resolve', :url => soundcloud_url, client_id: ENV['SOUNDCLOUD_SERVER_APP_ID'])
-    tracks = client.get("/users/#{station.id}/tracks")
+    client     = soundcloud_client
+    station    = client.get('/resolve', :url => soundcloud_url, client_id: ENV['SOUNDCLOUD_SERVER_APP_ID'])
+    tracks     = client.get("/users/#{station.id}/tracks")
 
     tracks.each do |track|
       if track.downloadable
-        item = Item.new
-        item.collection = collection
-        item.date_created = track.created_at
-        item.title = track.title
-        item.description = track.description
+        
+        identifier = track.uri
+        next if Item.where(identifier: identifier, collection_id: collection.id).exists?
 
-        instance = item.instances.build
-        instance.digital    = true
-        instance.format     = track.original_format
-        instance.identifier = track.id
+        item = Item.new
+        item.identifier   = identifier
+        item.collection   = collection
+        item.date_created = track.created_at
+        item.title        = track.title
+        item.description  = track.description
 
         audio = AudioFile.new
-        instance.audio_files << audio
+        audio.identifier      = track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
+        audio.remote_file_url = track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
         item.audio_files << audio
-        audio.identifier        =  track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
-        audio.remote_file_url   =  track.download_url+"?client_id="+ENV['SOUNDCLOUD_SERVER_APP_ID']
+
         item.save!
       end
     end
+  end
+
+  def self.soundcloud_client
+    Soundcloud.new({
+      :client_id     => ENV['SOUNDCLOUD_SERVER_APP_ID'] ,
+      :client_secret => ENV['SOUNDCLOUD_SERVER_APP_SECRET'],
+      :redirect_uri  => ENV['SOUNDCLOUD_POP_REDIRECT_URL']
+    })
   end
 
 end
