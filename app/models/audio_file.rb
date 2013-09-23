34a139aa63e1602fbaacaabf5d26046a9db5e6a7
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-09-23 16:12:05 -0400

    fix url params to pass through correctly

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -43,8 +43,8 @@ class AudioFile < ActiveRecord::Base
     fn
   end
 
-  def url(version={})
-    file.url(version) || original_file_url
+  def url(*args)
+    file.try(:url, *args) || original_file_url
   end
 
   def transcoded?
