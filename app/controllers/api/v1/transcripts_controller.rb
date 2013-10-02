b1f9663798143469701680b3f015068df164435a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-10-02 14:21:12 -0400

    add text transcripts - need to add link

--- app/controllers/api/v1/transcripts_controller.rb
+++ app/controllers/api/v1/transcripts_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::TranscriptsController < Api::V1::BaseController
   expose :audio_file
   expose(:transcript) { audio_file.timed_transcript }
 
-  respond_to :xml, :srt
+  respond_to :xml, :srt, :txt
 
   def show
     send_data render_to_string, disposition: %(attachment; filename="#{audio_file.filename}.transcript.#{params[:format]}")
