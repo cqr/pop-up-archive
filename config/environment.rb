77550779d397d168a58d0b439bc9d4e7a573f291
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-10-02 14:21:45 -0400

    add txt mime type for transcripts

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 Mime::Type.register "text/plain", :srt
+Mime::Type.register "text/plain", :txt
 
 # Initialize the rails application
 PopUpArchive::Application.initialize!
